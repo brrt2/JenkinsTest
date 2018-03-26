d7ac84dabfd3a4cc44d4b5df52049a5cfd36defc
Bartosz Pieczara <bartosz_pieczara@epam.com>
2018-03-26 10:47:02 +0200

    test 2

--- src/main/java/com/example/jenkinstest/HelloController.java
+++ src/main/java/com/example/jenkinstest/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
   @RequestMapping("/")
   public String index() {
-    return "Login test successful";
+    return "Login test successful after minor alterations";
   }
 
 }
